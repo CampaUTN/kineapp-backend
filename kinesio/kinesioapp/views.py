95a76b9f6ee753a28a407f1cb37ccdc470f44302
CampaUTN <rodrigocampassi@hotmail.com>
2019-06-11 22:55:25 -0300

    reverted migrations to new model

--- kinesio/kinesioapp/views.py
+++ kinesio/kinesioapp/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from django.views import generic
-from .models import Post
+from .models import Patient
 
 
 class IndexView(generic.View):
     def get(self, request):
-        return render(request, 'kinesioapp/index.html', {'posts': Post.objects.all()})
+        return render(request, 'kinesioapp/index.html', {'posts': Patient.objects.all()})
