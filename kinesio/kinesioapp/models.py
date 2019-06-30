bbf9871a8ed3036aac0911ac1c76998b4169ad96
Martin Kaynar <tinchokaynar@gmail.com>
2019-06-30 19:50:00 +0000

    Updated model

--- kinesio/kinesioapp/models.py
+++ kinesio/kinesioapp/models.py
@@ -16,16 +16,13 @@ class Medic(CUser):
 class Patient(CUser):
     start_date = models.DateTimeField()
     finish_date = models.DateTimeField()
+    current_medic = models.ForeignKey(Medic, on_delete=models.CASCADE, null=True)
 
 
-class Video(models.Model):
-    name = models.CharField(max_length=255)
-    owner = models.OneToOneField(Medic, on_delete=models.CASCADE)
-
-
-class Exercise(models.Model):
-    name = models.CharField(max_length=255)
-    videos = models.ForeignKey(Video, on_delete=models.CASCADE)
+class Homework(models.Model):
+    from_date = models.DateTimeField()
+    to_date = models.DateTimeField()
+    periodicity = models.IntegerField()
 
 
 class HomeworkExercise(models.Model):
@@ -38,34 +35,18 @@ class HomeworkExercise(models.Model):
     date = models.DateTimeField()
     number_of_homework_session = models.IntegerField()
     status = models.CharField(max_length=100, choices=HOMEWORK_SESSION_STATUS_CHOICES, default='PENDING')
-    exercises = models.ForeignKey(Exercise, on_delete=models.CASCADE)
+    homework = models.ForeignKey(Homework, on_delete=models.CASCADE, null=True)
 
 
-class Homework(models.Model):
-    from_date = models.DateTimeField()
-    to_date = models.DateTimeField()
-    periodicity = models.IntegerField()
-    exercises = models.ForeignKey(HomeworkExercise, on_delete=models.CASCADE)
+class Exercise(models.Model):
+    name = models.CharField(max_length=255)
+    homework_exercise = models.ForeignKey(HomeworkExercise, on_delete=models.CASCADE, null=True)
 
 
-class Image(models.Model):
-    content = models.CharField(max_length=255)
-    description = models.CharField(max_length=255)
-    date = models.DateTimeField()
-    homework = models.OneToOneField(Homework, on_delete=models.CASCADE)
-
-
-class Session(models.Model):
-    SESSION_STATUS_CHOICES = [
-        ('P', 'PENDING'),
-        ('F', 'FINISHED'),
-        ('C', 'CANCELLED')
-    ]
-    medic = models.ForeignKey(Medic, on_delete=models.CASCADE)
-    patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
-    date = models.DateTimeField()
-    status = models.CharField(max_length=100, choices=SESSION_STATUS_CHOICES, default='PENDING')
-    images = models.ForeignKey(Image, on_delete=models.CASCADE, blank=True, null=True)
+class Video(models.Model):
+    name = models.CharField(max_length=255)
+    owner = models.OneToOneField(Medic, on_delete=models.CASCADE)
+    exercise = models.ForeignKey(Exercise, on_delete=models.CASCADE, null=True)
 
 
 class ClinicalHistory(models.Model):
@@ -78,6 +59,25 @@ class ClinicalHistory(models.Model):
     date = models.DateTimeField()
     description = models.CharField(max_length=255)
     status = models.CharField(max_length=100, choices=CLINICAL_HISTORY_STATUS_CHOICES, default='PENDING')
-    sessions = models.ForeignKey(Session, on_delete=models.CASCADE)
     patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
-    medic = models.ForeignKey(Medic, on_delete=models.CASCADE)
+
+
+class ClinicalSession(models.Model):
+    SESSION_STATUS_CHOICES = [
+        ('P', 'PENDING'),
+        ('F', 'FINISHED'),
+        ('C', 'CANCELLED')
+    ]
+    date = models.DateTimeField()
+    status = models.CharField(max_length=100, choices=SESSION_STATUS_CHOICES, default='PENDING')
+    homework = models.OneToOneField(Homework, on_delete=models.CASCADE, blank=True, null=True)
+    clinical_history = models.ForeignKey(ClinicalHistory, on_delete=models.CASCADE)
+
+
+class Image(models.Model):
+    content = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    date = models.DateTimeField()
+    homework = models.OneToOneField(Homework, on_delete=models.CASCADE)
+    clinical_session = models.ForeignKey(ClinicalSession, on_delete=models.CASCADE, null=True)
+
