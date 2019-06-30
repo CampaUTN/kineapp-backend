57df94f897b518a2b45785514d68d0576283c478
Martin Kaynar <tinchokaynar@gmail.com>
2019-06-30 21:03:25 +0000

    Added Medic and Patient to admin register

--- kinesio/kinesioapp/admin.py
+++ kinesio/kinesioapp/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Medic, Patient
+
+
+class MedicAdmin(admin.ModelAdmin):
+    list_display = ['username', 'name', 'last_name']
+
+
+class PatientAdmin(admin.ModelAdmin):
+    list_display = ['username', 'name', 'last_name']
+
+
+admin.site.register(Medic, MedicAdmin)
+admin.site.register(Patient, PatientAdmin)
