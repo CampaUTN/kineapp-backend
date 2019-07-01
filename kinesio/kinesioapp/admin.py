1ac2f13ec99ecf686554409ec71e8fb09303651e
Martin Kaynar <tinchokaynar@gmail.com>
2019-07-01 03:27:16 +0000

    Added Clinical History and Session to admun register

--- kinesio/kinesioapp/admin.py
+++ kinesio/kinesioapp/admin.py
@@ -1,16 +1,24 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Medic, Patient
+from .models import Medic, Patient, ClinicalHistory, ClinicalSession
 
 
 class MedicAdmin(admin.ModelAdmin):
-    list_display = ['username', 'name', 'last_name']
+    list_display = ['pk', 'username', 'name', 'last_name']
 
 
 class PatientAdmin(admin.ModelAdmin):
-    list_display = ['username', 'name', 'last_name']
+    list_display = ['pk', 'username', 'name', 'last_name']
+
+class ClinicalHistoryAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'description', 'status']
+
+class ClinicalSessionAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'status']
 
 
 admin.site.register(Medic, MedicAdmin)
 admin.site.register(Patient, PatientAdmin)
+admin.site.register(ClinicalHistory, ClinicalHistoryAdmin)
+admin.site.register(ClinicalSession, ClinicalSessionAdmin)
