5772b0ee2b28faa8b2b8a61202a598a40082b44b
Martin Kaynar <tinchokaynar@gmail.com>
2019-07-08 19:39:21 -0300

    Login google URL

--- kinesio/kinesioapp/api.py
+++ kinesio/kinesioapp/api.py
@@ -5,6 +5,7 @@ from rest_framework.views import APIView
 from django.contrib.auth import authenticate
 from rest_framework import generics
 from google.oauth2 import id_token
+from google.auth.transport import requests
 
 from .models import Medic, ClinicalHistory, ClinicalSession, Patient, CUser
 from .serializers import MedicSerializer, ClinicalHistorySerializer, ClinicalSessionSerializer, PatientSerializer
@@ -61,24 +62,27 @@ class ClinicalSessionAPIView(generics.ListCreateAPIView):
 
 class TokenGoogleAPIView(APIView):
     def post(self, request):
-        google_token = request.POST['google_token']
+        google_token = request.data.get('google_token', None)
+        if google_token is None:
+            return Response({'error': 'Missing token'}, status=status.HTTP_404_NOT_FOUND)
+        else:
+            print(google_token)
+            try:
+                id_info = id_token.verify_oauth2_token(
+                    google_token,
+                    requests.Request(),
+                    '1093191472549-9gk2os2g3hm2qa1bhrhr1ab0cl7r5qkb.apps.googleusercontent.com')
 
-        try:
-            id_info = id_token.verify_oauth2_token(
-                google_token,
-                request.Request(),
-                '1093191472549-9gk2os2g3hm2qa1bhrhr1ab0cl7r5qkb.apps.googleusercontent.com')
+                if id_info['iss'] not in ['accounts.google.com', 'https://accounts.google.com']:
+                    return Response({'error': 'Invalid Issuer'}, status=status.HTTP_404_NOT_FOUND)
 
-            if id_info['iss'] not in ['accounts.google.com', 'https://accounts.google.com']:
-                return Response({'error': 'Invalid Issuer'}, status=status.HTTP_404_NOT_FOUND)
+                user_id = id_info['sub']
 
-            user_id = id_info['sub']
-
-            query_user = CUser.objects.filter(id_google=user_id)
-            if query_user.count() < 1:
-                return Response({'warning': 'User do not exist.'}, status=status.HTTP_206_PARTIAL_CONTENT)
-            else:
-                #FIXME Cambiar para que devuelva las preguntas cuando el ISSUE 94 este terminado
-                return Response({'questions': 'Saraza!'}, status=status.HTTP_200_OK)
-        except ValueError:
-            return Response({'error': 'Invalid Token. Please verify'}, status=status.HTTP_404_NOT_FOUND)
+                query_user = CUser.objects.filter(id_google=user_id)
+                if query_user.count() < 0:
+                    return Response({'warning': 'User do not exist.'}, status=status.HTTP_206_PARTIAL_CONTENT)
+                else:
+                    #FIXME Cambiar para que devuelva las preguntas cuando el ISSUE 94 este terminado
+                    return Response({'questions': 'Saraza!'}, status=status.HTTP_200_OK)
+            except ValueError:
+                return Response({'error': 'Invalid Token. Please verify'}, status=status.HTTP_404_NOT_FOUND)
