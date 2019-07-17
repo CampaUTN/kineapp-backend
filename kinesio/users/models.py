6283bad9d576be01ec6c7cb181a680d742bb242a
Rodrigo Campassi <CampaUTN@users.noreply.github.com>
2019-07-16 22:36:19 -0300

    Limit to fail login (#17)

--- kinesio/users/models.py
+++ kinesio/users/models.py
@@ -53,7 +53,7 @@ class User(AbstractUser):
     # FIXME Sacar null true cuando el ISSUE 93 este terminado
     id_google = models.CharField(max_length=100, db_index=True, null=True, blank=True, default=None)
     secret_question = models.ForeignKey(SecretQuestion, null=True, on_delete=models.SET_NULL)
-
+    tries = models.IntegerField(default=0)
     objects = UserManager()
 
     def __str__(self):
