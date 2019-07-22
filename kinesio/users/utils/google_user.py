978ef5d342c0711e5c9100fd8e25fe05b2d8ab4d
Martin Kaynar <tinchokaynar@gmail.com>
2019-07-22 20:41:03 -0300

    Added multiple applications support, fix error on token validation

--- kinesio/users/utils/google_user.py
+++ kinesio/users/utils/google_user.py
@@ -1,5 +1,6 @@
 from google.oauth2 import id_token
 from google.auth.transport import requests
+from django.conf import settings
 
 
 class InvalidTokenException(Exception):
@@ -12,19 +13,22 @@ class GoogleUser:
 
     def _validate_and_generate_account_information(self, google_token):
         try:
-            account_information = id_token.verify_oauth2_token(google_token,
-                                                               requests.Request(),
-                                                               '407408718192.apps.googleusercontent.com')
+
+            account_information = id_token.verify_oauth2_token(google_token, requests.Request())
+
+            if account_information['aud'] not in [settings.CLIENT_ID_ANDROID, settings.CLIENT_ID_WEB]:
+                raise ValueError('Could not verify audience.')
+            elif not self._token_is_valid(account_information):
+                raise InvalidTokenException
+            else:
+                return account_information
+
         except ValueError:
             raise InvalidTokenException
-        else:
-            if not self._token_is_valid:
-                raise InvalidTokenException
-        return account_information
 
-    @property
-    def _token_is_valid(self):
-        return all(key in self.account_information for key in ['iss', 'sub', 'given_name', 'family_name', 'email'])
+    @staticmethod
+    def _token_is_valid(acc_info):
+        return all(key in acc_info for key in ['iss', 'sub', 'given_name', 'family_name', 'email'])
 
     @property
     def username_is_valid(self):
