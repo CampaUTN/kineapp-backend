8f7d0a962ff1552557ef2976ffc1a202ef495c7c
Martin Kaynar <tinchokaynar@gmail.com>
2019-07-25 18:38:36 -0300

    New service get_clinical_histories, added urls for views

--- kinesio/kinesioapp/urls.py
+++ kinesio/kinesioapp/urls.py
@@ -4,8 +4,12 @@ from . import views, api
 
 urlpatterns = [
     re_path(r'^$', views.IndexView.as_view(), name='index'),
+    re_path(r'^logout/?$', views.logout_view, name='logout_view'),
     re_path(r'^secret_questions/?$', views.SecretQuestionView.as_view(), name='secret_questions_view'),
+    re_path(r'^no_user/?$', views.NoUserView.as_view(), name='no_user_view'),
+    re_path(r'^clinical_history/?$', views.ClinicalHistoryView.as_view(), name='clinical_history_view'),
     re_path(r'^api/v1/clinical_histories/?$', api.ClinicalHistoryAPIView.as_view(), name='clinical_history'),
+    re_path(r'^api/v1/get_clinical_histories/?$', api.get_clinical_histories, name='get_clinical_histories'),
     re_path(r'^api/v1/clinical_sessions/?$', api.ClinicalSessionAPIView.as_view(), name='clinical_session'),
     re_path(r'^api/v1/image/?$', api.upload_image, name='image'),
     re_path(r'^api/v1/image/(?P<pk>[0-9]+)$', api.ImageDetailAPIView.as_view(), name='image_delete'),
