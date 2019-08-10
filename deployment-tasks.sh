f7ed2bb353287969b581d31d4e7536c580a3241e
Martin Kaynar <tinchokaynar@gmail.com>
2019-08-10 01:29:46 -0300

    Fix: now deployment task execute also migrations for users ap

--- deployment-tasks.sh
+++ deployment-tasks.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo Creando migrations
-python kinesio/manage.py makemigrations kinesioapp
+python kinesio/manage.py makemigrations users kinesioapp
 
 echo Comienzo de migracion
 python kinesio/manage.py migrate
