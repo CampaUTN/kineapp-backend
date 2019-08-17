4d6abc2cba74327fdceca3a27060198eb4c818fd
Lucas <espositolucas95@gmail.com>
2019-08-17 18:57:34 -0300

    split test.py into multiple files

---
+++ kinesio/kinesioapp/tests/test_image.py
@@ -0,0 +1,48 @@
+from rest_framework import status
+
+from ..utils.test_utils import APITestCase
+from .. import models
+from ..models import ClinicalSession, Image
+from users.models import User
+
+
+class TestImageAPI(APITestCase):
+    def setUp(self) -> None:
+        self.medic = User.objects.create_user(username='juan', password='12345', first_name='juan',
+                                              last_name='gomez', license='matricula #15433')
+        self.patient = User.objects.create_user(first_name='facundo', last_name='perez', username='pepe',
+                                                password='12345', current_medic=self.medic)
+        self.clinical_session = ClinicalSession.objects.create(status=models.PENDING,
+                                                               patient=self.patient.patient)
+        self._log_in(self.medic, '12345')
+
+    def test_image_data_on_database_is_different_than_input(self):
+        content = b'content'
+        image = Image.objects.create(content=content, clinical_session=self.clinical_session)
+        self.assertNotEquals(image._content, content)
+
+    def test_create_image(self):
+        with open('/kinesio/kinesio/static/images/logo.png', 'rb') as file:
+            data = {'content': file, 'clinical_session_id': self.clinical_session.pk}
+            response = self.client.post('/api/v1/image/', data)
+        # Open the file again
+        with open('/kinesio/kinesio/static/images/logo.png', 'rb') as file:
+            content = file.read()
+        self.assertEquals(response.status_code, status.HTTP_201_CREATED)
+        self.assertEquals(Image.objects.count(), 1)
+        self.assertEquals(Image.objects.get().content, content)
+
+    def test_delete_image(self):
+        image = Image.objects.create(content=b'content', clinical_session=self.clinical_session)
+        response = self.client.delete(f'/api/v1/image/{image.id}')
+        self.assertEquals(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEquals(Image.objects.count(), 0)
+
+    def test_get_image(self):
+        with open('/kinesio/kinesio/static/images/logo.png', 'rb') as file:
+            content = file.read()
+        image = Image.objects.create(content=content, clinical_session=self.clinical_session)
+        response = self.client.get(f'/api/v1/image/{image.id}')
+        self.assertEquals(response.status_code, status.HTTP_200_OK)
+        self.assertEquals(type(response.content), bytes)
+        self.assertEquals(response.content, content)
