25caf3038d8dce73580f810b316da75e12604743
Lucas <espositolucas95@gmail.com>
2019-08-17 17:11:42 -0300

    changed the image of the test api to a jpg

--- kinesio/kinesioapp/api.py
+++ kinesio/kinesioapp/api.py
@@ -125,6 +125,6 @@ class ImageCreateAPIView(APIView):
 @api_view(["GET"])
 @permission_classes((AllowAny,))
 def image_hardcoded(request, id):
-    with open('/kinesio/kinesio/static/images/logo.png', 'rb') as file:
+    with open('/kinesio/kinesio/static/images/tmp.jpg', 'rb') as file:
         content = file.read()
-    return download(f'image_{id}.png', content)
+    return download(f'image_{id}.jpg', content)
