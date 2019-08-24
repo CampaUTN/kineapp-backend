141d441717df98ba6e8a22235398f596ca31cc61
lucasesposito <espositolucas95@gmail.com>
2019-08-24 10:32:07 -0300

    removed status from clinical session

--- kinesio/kinesioapp/models.py
+++ kinesio/kinesioapp/models.py
@@ -48,7 +48,6 @@ class ClinicalSessionQuerySet(models.QuerySet):
 
 class ClinicalSession(models.Model):
     date = models.DateTimeField(auto_now_add=True)
-    status = models.CharField(max_length=20, choices=choices.sessions.get(), default=choices.sessions.PENDING)
     # Fixme: uncomment when necessary: homework = models.OneToOneField(Homework, on_delete=models.CASCADE, blank=True, null=True)
     patient = models.ForeignKey(Patient, related_name='sessions', on_delete=models.CASCADE)
 
