2e9e09753c96a0e2cf91ca88cf2b7844672ac1f1
Lucas <espositolucas95@gmail.com>
2019-09-22 03:16:11 -0300

    reworked login endpoint responses, better responses for token errors

--- kinesio/users/tests/test_questions.py
+++ kinesio/users/tests/test_questions.py
@@ -40,26 +40,24 @@ class CheckAnswerAPI(APITestCase):
             "secret_question_id": self.question.id,
             "answer": "azul"
         })
-        self.assertEqual(response.status_code, status.HTTP_406_NOT_ACCEPTABLE)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
-    def test_answer_not_found_user_id(self):
+    def test_invalid_token(self):
         self.user.delete()
         response = self.client.post('/api/v1/login/', {
-            "google_token": "anotherUser",
+            "google_token": "random invalid token",
             "secret_question_id": self.question.id,
             "answer": "rojo"
         })
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-        self.assertEqual(response.json().get("message"), "User not found")
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
-    def test_answer_not_found_question_id(self):
+    def test_question_id_not_found(self):
         response = self.client.post('/api/v1/login/', {
             "google_token": self.user.username,
             "secret_question_id": 879879,
             "answer": "rojo"
         })
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-        self.assertEqual(response.json().get("message"), "Question not found")
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
     def test_login_fail_max_times(self):
         for x in range(0, int(settings.MAX_PASSWORD_TRIES) + 1):
--- kinesio/users/utils/google_user.py
+++ kinesio/users/utils/google_user.py
@@ -1,9 +1,18 @@
 from google.oauth2 import id_token
 from google.auth.transport import requests
 from django.conf import settings
+import textwrap
 
 
-class InvalidTokenException(Exception):
+class InformationNotAccessibleFromTokenException(Exception):
+    pass
+
+
+class GoogleRejectsTokenException(Exception):
+    pass
+
+
+class InvalidAudienceException(Exception):
     pass
 
 
@@ -13,8 +22,12 @@ class GoogleUser:
 
     def _validate_and_generate_account_information(self, google_token):
         account_information = self._get_account_information(google_token)
-        if account_information['aud'] not in [settings.CLIENT_ID_ANDROID, settings.CLIENT_ID_WEB] or not self._account_information_is_valid(account_information):
-            raise InvalidTokenException
+        if account_information['aud'] not in [settings.CLIENT_ID_ANDROID, settings.CLIENT_ID_WEB]:
+            raise InvalidAudienceException('Audience is neither the client_id of the web or the mobile application.')
+        if not self._account_information_is_valid(account_information):
+            raise InformationNotAccessibleFromTokenException(textwrap.dedent(""" Some information is not accesible with the given token.
+                                                                                 The back end needs access to the following fields:
+                                                                                 aud, iss, sub, given_name, family_name, email, picture. """))
         return account_information
 
     @staticmethod
@@ -26,7 +39,7 @@ class GoogleUser:
         try:
             account_information = id_token.verify_oauth2_token(google_token, requests.Request())
         except ValueError:
-            raise InvalidTokenException
+            raise GoogleRejectsTokenException('Google rejects the token.')
         return account_information
 
     @property
