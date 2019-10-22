05cf0c6168f459387766c9955ee4aa7ceac8917c
Lucas <espositolucas95@gmail.com>
2019-10-22 03:05:54 -0300

    do not allow registration with invalid secret question id, test for that case

--- kinesio/users/api/register.py
+++ kinesio/users/api/register.py
@@ -8,8 +8,9 @@ from drf_yasg import openapi
 from drf_yasg.utils import swagger_auto_schema
 from rest_framework.authtoken.models import Token
 from rest_framework.request import HttpRequest
+from django.shortcuts import get_object_or_404
 
-from ..models import User
+from ..models import User, SecretQuestion
 from ..serializers import UserSerializer, TokenSerializer
 from ..tests.utils.mock_decorators import mock_google_user_on_tests
 from ..utils.google_user import GoogleUser
@@ -49,6 +50,9 @@ from ..utils.google_user import GoogleUser
         ),
         status.HTTP_400_BAD_REQUEST: openapi.Response(
             description="Missing parameter or license and current_medic specified at the same time."
+        ),
+        status.HTTP_404_NOT_FOUND: openapi.Response(
+            description="Secret question not found."
         )
     }
 )
@@ -69,6 +73,7 @@ def register(request: HttpRequest, google_user_class: type = GoogleUser) -> Resp
         response = Response({'error': 'Do not specify current_medic and license at the same time'},
                             status=status.HTTP_400_BAD_REQUEST)
     else:
+        get_object_or_404(SecretQuestion, id=secret_question_id)  # Check secret question existence
         google_user = google_user_class(google_token)
         user_created = User.objects.create_user(username=google_user.user_id,
                                                 first_name=request.data.get('first_name', google_user.first_name),
--- kinesio/users/tests/test_registration.py
+++ kinesio/users/tests/test_registration.py
@@ -59,3 +59,11 @@ class TestRegistration(APITestCase):
         response = self.client.post('/api/v1/registration/', data, format='json')
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertEqual(User.objects.count(), 0)
+
+    def test_registration_fails_if_secret_question_does_not_exist(self):
+        data = {'google_token': 'i_am_a_working_token', 'license': '1234',
+                'dni': 39203040, 'birth_date': datetime.now().date(), 'answer': 'sky',
+                'secret_question_id': self.secret_question_id + 1}
+        response = self.client.post('/api/v1/registration/', data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+        self.assertEqual(User.objects.count(), 0)
