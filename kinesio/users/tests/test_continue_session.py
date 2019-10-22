1c438e2faf149aaf8ee5a2977c9b4edcb49a39f5
Lucas <espositolucas95@gmail.com>
2019-10-22 02:40:26 -0300

    the answer is case insensitive

--- kinesio/users/tests/test_continue_session.py
+++ kinesio/users/tests/test_continue_session.py
@@ -7,14 +7,14 @@ from ..models import User, SecretQuestion
 
 class TestContinueSession(APITestCase):
     def setUp(self) -> None:
-        self.secret_question = SecretQuestion.objects.create(description='The answer is 1234?')
-        self.answer = '1234'
+        self.secret_question = SecretQuestion.objects.create(description='The answer is sky?')
+        self.answer = 'sky'
         self.user = User.objects.create_user(username='facundo22', first_name='facundo', password=self.answer,
                                              secret_question=self.secret_question, dni=25000033,
                                              birth_date=datetime.now())
 
     def test_continue_session_with_valid_credentials(self):
-        self._log_in(self.user, '1234')
+        self._log_in(self.user, 'sky')
         data = {'secret_question_id': self.secret_question.id, 'answer': self.answer}
         response = self.client.post(f'/api/v1/continue_session/', data=data)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
@@ -25,19 +25,25 @@ class TestContinueSession(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
     def test_continue_session_with_invalid_question_id(self):
-        self._log_in(self.user, '1234')
+        self._log_in(self.user, 'sky')
         data = {'secret_question_id': self.secret_question.id + 1, 'answer': self.answer}
         response = self.client.post(f'/api/v1/continue_session/', data=data)
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
     def test_continue_session_with_invalid_answer(self):
-        self._log_in(self.user, '1234')
+        self._log_in(self.user, 'sky')
         data = {'secret_question_id': self.secret_question.id, 'answer': self.answer + 'a'}
         response = self.client.post(f'/api/v1/continue_session/', data=data)
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
     def test_continue_session_with_invalid_question_id_and_answer(self):
-        self._log_in(self.user, '1234')
+        self._log_in(self.user, 'sky')
         data = {'secret_question_id': self.secret_question.id + 100, 'answer': self.answer + 's'}
         response = self.client.post(f'/api/v1/continue_session/', data=data)
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+    
+    def test_continue_session_answer_is_case_insensitive(self):
+        self._log_in(self.user, 'sky')
+        data = {'secret_question_id': self.secret_question.id, 'answer': 'SKy'}
+        response = self.client.post(f'/api/v1/continue_session/', data=data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
