77f478a6787af566d9c96819615ca5ba407a57f7
Lucas <espositolucas95@gmail.com>
2019-10-22 01:56:48 -0300

    continue login endpoint

--- kinesio/users/api/continue_session.py
+++ kinesio/users/api/continue_session.py
@@ -1,39 +1,40 @@
 from rest_framework.response import Response
 from rest_framework import status
+from django.contrib import auth
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
 from django.views.decorators.csrf import csrf_exempt
 from drf_yasg import openapi
 from drf_yasg.utils import swagger_auto_schema
-from django.shortcuts import get_object_or_404
 from rest_framework.request import HttpRequest
+import textwrap
 
-from ..models import User
 from ..serializers import TokenSerializer
-from ..tests.utils.mock_decorators import mock_google_user_on_tests
-from ..utils.google_user import GoogleUser, GoogleRejectsTokenException, InformationNotAccessibleFromTokenException, InvalidAudienceException
 from .utils.authenticate import authenticate
 
 
 @swagger_auto_schema(
     method='post',
-    operation_id='login (2FA)',
+    operation_id='continue_session',
+    operation_description=textwrap.dedent(""" Similar to login endpoint. However, instead of requesting google's token,
+                                              this endpoint requires the user to be logged in.
+                                              This is not for login. It's to confirm session validity after the
+                                              automatic logout. """),
     request_body=openapi.Schema(
         type=openapi.TYPE_OBJECT,
         properties={
-            'username': openapi.Schema(type=openapi.TYPE_INTEGER),
             'secret_question_id': openapi.Schema(type=openapi.TYPE_INTEGER),
             'answer': openapi.Schema(type=openapi.TYPE_STRING)
         },
-        required=['username', 'secret_question_id', 'answer']
+        required=['secret_question_id', 'answer']
     ),
     responses={
         status.HTTP_200_OK: openapi.Response(
-            description="User logged in.",
+            description="User session will continue.",
             schema=TokenSerializer(),
         ),
         status.HTTP_400_BAD_REQUEST: openapi.Response(
-            description="Missing parameter, non-integer secret_question_id, error related to google token (see response's message for details)",
+            description="Missing parameter or non-integer secret_question_id (see response's message for details)",
             schema=openapi.Schema(
                 type=openapi.TYPE_OBJECT,
                 properties={
@@ -49,22 +50,12 @@ from .utils.authenticate import authenticate
 @csrf_exempt
 @api_view(["POST"])
 @permission_classes((AllowAny,))
-@mock_google_user_on_tests
-def login(request: HttpRequest, google_user_class: type = GoogleUser) -> Response:
-    # Check that there are no missing parameters
+def login(request: HttpRequest) -> Response:
+    # Check whether there are missing parameters
     try:
-        google_token = request.data['google_token']
         secret_question_id = int(request.data['secret_question_id'])
         answer = request.data['answer']
     except KeyError:
         return Response({'message': 'Missing parameter'}, status=status.HTTP_400_BAD_REQUEST)
-
-    # Check whether google token is valid
-    try:
-        google_user = google_user_class(google_token)
-    except (InformationNotAccessibleFromTokenException, GoogleRejectsTokenException, InvalidAudienceException) as exception:
-        return Response({'message': str(exception)}, status=status.HTTP_400_BAD_REQUEST)
     else:
-        # Check user existence
-        user = get_object_or_404(User, username=google_user.user_id)
-        return authenticate(request, user, secret_question_id, answer)
+        return authenticate(request, request.user, secret_question_id, answer)
