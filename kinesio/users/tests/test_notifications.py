21c18fe3e44dbc3e7054b71bbfd3f7f18e36aff1
Lucas <espositolucas95@gmail.com>
2019-10-31 01:37:21 -0300

    fixed tests

--- kinesio/users/tests/test_notifications.py
+++ kinesio/users/tests/test_notifications.py
@@ -14,19 +14,19 @@ class ChangeDeviceIDAPI(APITestCase):
 
     def test_device_id_changed_for_medic_user(self):
         self._log_in(self.medic_user, '1234')
-        response = self.client.post('/api/v1/login/', {'firebase_device_id': 'AAAA'})
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.user.refresh_from_db()
+        response = self.client.post('/api/v1/change_device_id/', {'firebase_device_id': 'AAAA'})
+        self.assertEqual(response.status_code, status.HTTP_202_ACCEPTED)
+        self.medic_user.refresh_from_db()
         self.assertEqual(self.medic_user.firebase_device_id, 'AAAA')
 
     def test_device_id_changed_for_patient_user(self):
         self._log_in(self.patient_user, '1234')
-        response = self.client.post('/api/v1/login/', {'firebase_device_id': 'BBBB'})
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.user.refresh_from_db()
+        response = self.client.post('/api/v1/change_device_id/', {'firebase_device_id': 'BBBB'})
+        self.assertEqual(response.status_code, status.HTTP_202_ACCEPTED)
+        self.patient_user.refresh_from_db()
         self.assertEqual(self.patient_user.firebase_device_id, 'BBBB')
 
     def test_device_id_change_endpoint_fails_without_device_id(self):
         self._log_in(self.patient_user, '1234')
-        response = self.client.post('/api/v1/login/', {})
+        response = self.client.post('/api/v1/change_device_id/', {})
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
