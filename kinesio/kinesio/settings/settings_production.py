bee612d052c36eb40a3ff53cc2e9d3403850dee1
Lucas <espositolucas95@gmail.com>
2019-11-01 02:49:46 -0300

    added settings_production.py again

---
+++ kinesio/kinesio/settings/settings_production.py
@@ -0,0 +1,8 @@
+# String. Obtained from Firebase.
+FIREBASE_API_KEY = ''
+
+# Bytes. Generate it manually.
+IMAGE_ENCRYPTION_KEY = b'k-rE9SGW0vOCK7aBDPwBHhb0fhJVsGA-hpsxXCWOB9o='
+
+# Django's secret key. String. Generate it manually.
+SECRET_KEY = 'lex&^&d#s^yuz-y#g+5=nr%t+&r7i)xtwjl=^mamo(l%&p2ql4'
